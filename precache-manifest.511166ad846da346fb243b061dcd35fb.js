effca2e88131ff8da07915b75e00de74409b090f
Ada Void <oxiggy@oxiggy.com>
2020-01-11 11:00:41 +0200

    сборка

---
+++ precache-manifest.511166ad846da346fb243b061dcd35fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c51ea68968f5c5e3a4e547c624e55ac4",
+    "url": "/linguacourse-tracker/index.html"
+  },
+  {
+    "revision": "5fc45c31da6063d66975",
+    "url": "/linguacourse-tracker/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "912c72ada1e41b021a71",
+    "url": "/linguacourse-tracker/static/js/2.3aca2742.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/linguacourse-tracker/static/js/2.3aca2742.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5fc45c31da6063d66975",
+    "url": "/linguacourse-tracker/static/js/main.aaaa179e.chunk.js"
+  },
+  {
+    "revision": "15e838583deff3c2c495",
+    "url": "/linguacourse-tracker/static/js/runtime-main.bbebc9af.js"
+  }
+]);
